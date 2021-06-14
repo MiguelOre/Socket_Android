621fb8a28484ef669984e60a400b5fa72dd0695e
Miguel Angel Ore Quintana <maoq_2009@hotmail.com>
2021-06-13 20:35:18 -0500

    Initial commit

---
+++ AndroidSocketCLIENT/gen/com/tdproyect/android/androidsocketclient/BuildConfig.java
@@ -0,0 +1,6 @@
+
+package com.tdproyect.android.androidsocketclient;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidSocketSERVER/gen/com/tdproyect/android/androidsocketserver/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tdproyect.android.androidsocketserver;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
